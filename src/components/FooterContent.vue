bd9d311735fcd4eb10519a1ab70ecf2b719c904a
Matteogatto94 <cosenza94@live.it>
2023-02-10 16:10:44 +0100

    add footer

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -5,9 +5,31 @@ export default {
 </script>
 
 <template>
-    <h1>Questo è il mio footer</h1>
+    <footer>
+        <div class="footer_contacts d-flex justify-content-between p-4">
+            <div class="contacts">
+                <a href="/">&copy; 2023 Boolbnb, Inc.</a> ·
+                <a href="/">Privacy</a> ·
+                <a href="/">Termini</a> ·
+                <a href="/">Mappa del sito</a> ·
+                <a href="/">Dettagli dell'azienda</a> ·
+                <a href="/">Destinazioni</a>
+            </div>
+            <div class="languages">
+                <a href="/">Italiano (IT)</a> ·
+                <a href="/">€ EUR</a> ·
+                <a href="/">Supporto e risorse</a>
+            </div>
+        </div>
+    </footer>
+
 </template>
 
 <style lang="scss" scoped>
-
+.footer_contacts {
+    a {
+        text-decoration: none;
+        color: black;
+    }
+}
 </style>
