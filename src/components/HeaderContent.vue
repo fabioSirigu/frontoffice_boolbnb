b8652567ca93a3f75b6d15e5b400c24c0331cbba
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-10 16:11:15 +0100

    Fixed header, wip responsive one

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -5,21 +5,41 @@ export default {
 </script>
 
 <template>
-<div class="header_wrapper p-2">
+<div class="header_wrapper p-4">
     <div class="header_elements">
         <div class="row justify-content-around align-items-center">
-            <div class="col-4 left">
-                <div class="logo_wrapper">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 left d-flex justify-content-start">
+                <div class="logo_wrapper px-5">
                     <img class="header_logo" src="../assets/img/logoBnBlateral.png" alt="Header branding">
                 </div>
             </div>
-            <div class="col-4 center">
-                <div class="search_wrapper">
-                    <input class="search_header" type="text" placeholder="Cerca il tuo posto..">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4 center">
+                <div class="search_wrapper d-flex justify-content-between align-items-center gap-3">
+                    <input class="search_header" type="text" placeholder="Dove ti porto?">
+                    <a class="search_button" type="submit"><span><i class="fa-solid fa-magnifying-glass px-2"></i></span>Ricerca</a>
                 </div>
             </div>
-            <div class="col-4 right">
-
+            <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4 right d-flex justify-content-end">
+                <div class="user_elements d-flex justify-content-between align-items-center gap-2 px-5">
+                    <a class="affitta_header" href="#">Affitta con BoolBnb</a>
+                    <div class="user_dropdown_wrapper">
+                        <div class="user_dropdown_elements">
+                            <div class="btn-group">
+                                <button type="button" class="btn user_header dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa-regular fa-user px-2"></i>
+                                    Il mio BoolBnb
+                                </button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Separated link</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -37,9 +57,67 @@ export default {
     padding: 10px;
     width: 100%;
     box-shadow: black;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.092);
 }
 
 .header_wrapper {
     width: 100%;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.099);
 }
+
+.search_button {
+    display: inline-block;
+    color: white;
+    background-color: #FF5A5F;
+    padding: 10px;
+    text-decoration: none;
+    border-radius: 20px;
+    width: 150px;
+}
+
+.search_button:hover {
+    transition: 0.4s;
+    color: #FF5A5F;
+    background-color: white;
+}
+
+.affitta_header {
+    display: inline-block;
+    color: rgb(0, 0, 0);
+    background-color: #ffffff;
+    padding: 10px;
+    text-decoration: none;
+    border-radius: 20px;
+    width: 200px;
+    text-align: center;
+}
+
+.affitta_header:hover {
+    transition: 0.4s;
+    color: #000000;
+    background-color: rgba(205, 205, 205, 0.173);
+}
+
+.user_header {
+    background-color: #FF5A5F;
+    color: white;
+    padding: 10px;
+    border-radius: 20px;
+}
+
+.user_header:hover {
+    transition: 0.4s;
+    color: #FF5A5F;
+    background-color: white;
+}
+
+.dropdown_user {
+    position: relative;
+}
+
+.dropdown_user_elements{
+    position: absolute;
+
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/scss/style.scss'
+import '../node_modules/bootstrap/js/src/dropdown';
 import App from './App.vue'
 
 createApp(App).mount('#app')
