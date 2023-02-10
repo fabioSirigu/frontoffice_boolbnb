7045b4a282f9023db783bf3704d8704544d2dde3
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-10 16:52:33 +0100

    Fixed header responsive, wip footer one

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -8,19 +8,19 @@ export default {
 <div class="header_wrapper p-4">
     <div class="header_elements">
         <div class="row justify-content-around align-items-center">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 left d-flex justify-content-start">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 left d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-start justify-content-xl-start justify-content-xxl-start">
                 <div class="logo_wrapper px-5">
                     <img class="header_logo" src="../assets/img/logoBnBlateral.png" alt="Header branding">
                 </div>
             </div>
-            <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4 center">
-                <div class="search_wrapper d-flex justify-content-between align-items-center gap-3">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 center">
+                <div class="search_wrapper d-flex justify-content-center align-items-center gap-3">
                     <input class="search_header" type="text" placeholder="Dove ti porto?">
                     <a class="search_button" type="submit"><span><i class="fa-solid fa-magnifying-glass px-2"></i></span>Ricerca</a>
                 </div>
             </div>
-            <div class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4 right d-flex justify-content-end">
-                <div class="user_elements d-flex justify-content-between align-items-center gap-2 px-5">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 right d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-end justify-content-xl-end justify-content-xxl-end">
+                <div class="user_elements d-flex justify-content-between align-items-center gap-1">
                     <a class="affitta_header" href="#">Affitta con BoolBnb</a>
                     <div class="user_dropdown_wrapper">
                         <div class="user_dropdown_elements">
@@ -29,12 +29,9 @@ export default {
                                     <i class="fa-regular fa-user px-2"></i>
                                     Il mio BoolBnb
                                 </button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Separated link</a>
+                                <div class="dropdown-menu dropdown_user">
+                                    <a class="dropdown-item dropdown_user_item" href="#">Login</a>
+                                    <a class="dropdown-item dropdown_user_item" href="#">Registrati ora</a>
                                 </div>
                             </div>
                         </div>
@@ -56,7 +53,6 @@ export default {
     border-radius: 20px;
     padding: 10px;
     width: 100%;
-    box-shadow: black;
     box-shadow: 0 3px 10px rgba(0, 0, 0, 0.092);
 }
 
@@ -118,6 +114,20 @@ export default {
 .dropdown_user_elements{
     position: absolute;
 
+}
+
+.dropdown_user {
+    border: 1px solid rgba(0, 0, 0, 0.081);
+    border-radius: 20px;
+    padding: 10px;
+    width: 100%;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.033);
+}
+
+.dropdown_user_item:hover {
+    background-color: white;
+    color: #FF5A5F;
+    transition: 0.4s;
 }
 
 </style>
