8d7c4b9cd62f6c02f696e6bb69d89233b0263c21
Matteogatto94 <cosenza94@live.it>
2023-02-10 15:19:15 +0100

    add footer components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderContent from './components/HeaderContent.vue'
+import FooterContent from './components/FooterContent.vue'
 </script>
 
 <template>
   <HeaderContent></HeaderContent>
+  <FooterContent></FooterContent>
 </template>
 
 <style scoped>
---
+++ src/components/FooterContent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'FooterContent'
+}
+</script>
+
+<template>
+    <h1>Questo è il mio footer</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
