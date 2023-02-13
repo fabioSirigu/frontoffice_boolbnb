589312cd9147565f788c75ce40a97d8e7b3f97fb
Yuri-Punzo <yuripunzo@hotmail.it>
2023-02-13 15:48:30 +0100

    fixed search v-if error "nessuna corrispondeza"

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -9,7 +9,7 @@ export default {
     props: [
         'filteredHomes'
     ],
-    methods : {
+    methods: {
         imageConverter(way) {
             console.log(way);
             if (way) {
@@ -18,16 +18,16 @@ export default {
             return "https://htmlcolors.com/brand-image/airbnb.png";
         },
         trimBody(text) {
-        if (text.length > this.max) {
-            return text.slice(0, this.max) + '...'
-        }
-        return text
+            if (text.length > this.max) {
+                return text.slice(0, this.max) + '...'
+            }
+            return text
         }
     },
     data() {
         return {
             api_url: "http://127.0.0.1:8000",
-            loading : false,
+            loading: false,
             max: 30,
         }
     }
@@ -35,60 +35,65 @@ export default {
 </script>
 
 <template>
-<div class="main_wrapper p-3">
-    <div class="main_elements">
-        <div class="titles_wrapper">
-            <div class="titles_elements text-center">
-                <h1 class="main_title black">
-                    Cerchi Casa? <br>
-                    <span class="fancy">BoolBnb</span>.
-                </h1>
-                <p>
-                    Con <span class="bold">BoolBnb</span>, puoi affittare la tua casa ideale <span class="bold">ovunque tu voglia</span>, <span class="bold">quando vuoi</span>. <br>
-                    Siamo il primo sistema di affitto case al mondo che ti offre una gamma completa di opzioni, <br> inoltre, siamo orgogliosi di offrire un'esperienza di prenotazione semplice e intuitiva, <br>
-                    che ti permette di prenotare la tua casa ideale in pochi clic.
-                </p>
+    <div class="main_wrapper p-3">
+        <div class="main_elements">
+            <div class="titles_wrapper">
+                <div class="titles_elements text-center">
+                    <h1 class="main_title black">
+                        Cerchi Casa? <br>
+                        <span class="fancy">BoolBnb</span>.
+                    </h1>
+                    <p>
+                        Con <span class="bold">BoolBnb</span>, puoi affittare la tua casa ideale <span
+                            class="bold">ovunque tu voglia</span>, <span class="bold">quando vuoi</span>. <br>
+                        Siamo il primo sistema di affitto case al mondo che ti offre una gamma completa di opzioni, <br>
+                        inoltre, siamo orgogliosi di offrire un'esperienza di prenotazione semplice e intuitiva, <br>
+                        che ti permette di prenotare la tua casa ideale in pochi clic.
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class="search_wrapper">
-            <div class="search_elements">
-                <div class="row align-items-center align-content-start">
-                    <div v-if="loading && filteredHomes.length === 0">
-                        <div class="alert alert-danger alert-dismissible fade show d-flex justify-content-between align-items-center" role="alert">
-                            <p class="p-0 m-0">
-                                Nessuna corrispondenza.
-                            </p>
-                            <button type="button" class="close btn close_button_alert" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+            <div class="search_wrapper">
+                <div class="search_elements">
+                    <div class="row align-items-center align-content-start">
+                        <div v-if="!loading && filteredHomes.length === 0">
+                            <div class="alert alert-danger alert-dismissible fade show d-flex justify-content-between align-items-center"
+                                role="alert">
+                                <p class="p-0 m-0">
+                                    Nessuna corrispondenza.
+                                </p>
+                                <button type="button" class="close btn close_button_alert" data-dismiss="alert"
+                                    aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row align-items-center align-content-start" v-else>
-                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 text-center " v-for="home in filteredHomes">
-                            <div class="single_home_contents p-2">
-                                <img class="home_image" :src="imageConverter(home.cover_image)">
-                                <div class="card-body justify-content-center mt-2">
-                                    <h5 class="card_title bold card-title text-center py-3">
-                                        {{ trimBody(home.title) }}
-                                    </h5>
+                        <div class="row align-items-center align-content-start" v-else>
+                            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 text-center "
+                                v-for="home in filteredHomes">
+                                <div class="single_home_contents p-2">
+                                    <img class="home_image" :src="imageConverter(home.cover_image)">
+                                    <div class="card-body justify-content-center mt-2">
+                                        <h5 class="card_title bold card-title text-center py-3">
+                                            {{ trimBody(home.title) }}
+                                        </h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="homes_wrapper">
-            <h1 class="secondary_title text-center black p-4">
-                In primo piano
-            </h1>
-            <SingleHomeContent></SingleHomeContent>
-            <div class="show_more_cta d-flex justify-content-center">
-                <a class="show_button" href="#">Tutte le case</a>
+            <div class="homes_wrapper">
+                <h1 class="secondary_title text-center black p-4">
+                    In primo piano
+                </h1>
+                <SingleHomeContent></SingleHomeContent>
+                <div class="show_more_cta d-flex justify-content-center">
+                    <a class="show_button" href="#">Tutte le case</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <style lang="scss" scoped>
@@ -121,9 +126,9 @@ export default {
 }
 
 .show_button:hover {
-  transition: 0.4s;
-  color: #ff5a5f;
-  background-color: white;
+    transition: 0.4s;
+    color: #ff5a5f;
+    background-color: white;
 }
 
 .close_button_alert {
