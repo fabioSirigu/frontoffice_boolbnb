7a06e87d293b0265db50c17c5491be544ae99276
Yuri-Punzo <yuripunzo@hotmail.it>
2023-02-13 22:46:20 +0100

    added auto complete search - needs aesthetyc fix

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,55 +1,73 @@
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
   name: "Header Content",
   data() {
     return {
-        api_url: "http://127.0.0.1:8000",
-        query: '',
-        latitude: '',
-        longitude: '',
-        radius: 20,
-        filteredHomes: [],
-        authenticated: false,
-        loading : false,
-        name: ''
+      api_url: "http://127.0.0.1:8000",
+      query: '',
+      addresses: [],
+      latitude: '',
+      longitude: '',
+      radius: 20,
+      filteredHomes: [],
+      authenticated: false,
+      loading: false,
+      name: ''
     };
   },
   methods: {
+    async searchAddress() {
+      try {
+        const response = await axios.get(
+          /* `https://api.tomtom.com/search/2/search/${this.query}.json?key=MkDmWSoUG13t3UpcLNV0AcyG6GRL8SlT` */
+          /* https://{baseURL}/search/{versionNumber}/search/{query}.{ext}?key={Your_API_Key}&typeahead={typeahead}&limit={limit}&ofs={ofs}&countrySet={countrySet}&lat={lat}&lon={lon}&radius={radius}&topLeft={topLeft}&btmRight={btmRight}&language={language}&idxSet={idxSet}&extendedPostalCodesFor={extendedPostalCodesFor}&minFuzzyLevel={minFuzzyLevel}&maxFuzzyLevel={maxFuzzyLevel}&categorySet={categorySet}&brandSet={brandSet}&connectorSet={connectorSet}&fuelSet={fuelSet}&view={view}&openingHours={openingHours}&timeZone={timeZone}&mapcodes={mapcodes}&relatedPois={relatedPois}&minPowerKW={minPowerKW}&maxPowerKW={maxPowerKW}&entityTypeSet={entityTypeSet} */
+          `https://api.tomtom.com/search/2/search/${this.query}.json?key=MkDmWSoUG13t3UpcLNV0AcyG6GRL8SlT&typeahead=true&countrySet=IT&language=it-IT`
+          /* `https://api.tomtom.com/search/2/autocomplete/${this.query}.json?key=MkDmWSoUG13t3UpcLNV0AcyG6GRL8SlT&language=it-IT&countrySet=IT/ITA` */
+        );
+        this.addresses = response.data.results;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    selectAddress(address) {
+      this.query = address.address.freeformAddress;
+      this.addresses = [];
+    },
     async searchHomes() {
-        if (this.query.length >= 0) {
-            const tomtomApiKey = '1W1nNbKly7WXl6NvYnr7983RJJawL26E';
-            const response = await axios.get(
-            `https://api.tomtom.com/search/2/geocode/${this.query}.JSON?key=${tomtomApiKey}`
-            );
-            const data = response.data;
-            if (data.results.length > 0) {
-            this.latitude = data.results[0].position.lat;
-            this.longitude = data.results[0].position.lon;
-            console.log(this.latitude);
-            console.log(this.longitude);
-            }
-
-            // Recupera le case tramite la chiamata API al backend
-            try {
-            const homesResponse = await axios.get(
-                'http://127.0.0.1:8000/api/homes/' +
-                this.latitude +
-                '/' +
-                this.longitude +
-                '/' +
-                this.radius
-            );
-            console.log(this.filteredhomes = homesResponse.data.data);
-            this.loading = true
-            this.$emit('search-homes-completed', homesResponse.data.data);
-
-            } catch (error) {
-            console.error(error);
-            }
+      if (this.query.length >= 0) {
+        const tomtomApiKey = '1W1nNbKly7WXl6NvYnr7983RJJawL26E';
+        const response = await axios.get(
+          `https://api.tomtom.com/search/2/geocode/${this.query}.JSON?key=${tomtomApiKey}`
+        );
+        const data = response.data;
+        if (data.results.length > 0) {
+          this.latitude = data.results[0].position.lat;
+          this.longitude = data.results[0].position.lon;
+          console.log(this.latitude);
+          console.log(this.longitude);
         }
+
+        // Recupera le case tramite la chiamata API al backend
+        try {
+          const homesResponse = await axios.get(
+            'http://127.0.0.1:8000/api/homes/' +
+            this.latitude +
+            '/' +
+            this.longitude +
+            '/' +
+            this.radius
+          );
+          console.log(this.filteredhomes = homesResponse.data.data);
+          this.loading = true
+          this.$emit('search-homes-completed', homesResponse.data.data);
+
+        } catch (error) {
+          console.error(error);
         }
+      }
+    }
   },
   mounted() {
     axios.get(this.api_url + '/api/user')
@@ -70,77 +88,51 @@ export default {
   <div class="header_wrapper p-4">
     <div class="header_elements">
       <div class="row justify-content-around align-items-center">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4 col-xxl-4 left d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-start justify-content-xxl-start left">
+        <div
+          class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4 col-xxl-4 left d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-start justify-content-xxl-start left">
           <div class="logo_wrapper px-5">
-            <img
-              class="header_logo"
-              src="../assets/img/logoBnBlateral.png"
-              alt="Header branding"
-            />
-          </div>
-        </div>
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4 col-xxl-4 justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-start justify-content-xxl-start center">
-          <div
-            class="search_wrapper d-flex justify-content-center align-items-center gap-3"
-          >
-            <input class="search_header" type="text" placeholder="Dimmi una Città o un Indirizzo.." v-model="query" @keyup.enter="searchHomes()"
-            />
-            <a class="search_button" @click="searchHomes()"
-              ><span><i class="fa-solid fa-magnifying-glass px-2"></i></span
-              >Ricerca</a
-            >
+            <img class="header_logo" src="../assets/img/logoBnBlateral.png" alt="Header branding" />
           </div>
         </div>
         <div
-          class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4 col-xxl-4 right d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-end justify-content-xxl-end"
-        >
-          <div
-            class="user_elements d-flex justify-content-between align-items-center gap-1"
-          >
+          class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4 col-xxl-4 justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-start justify-content-xxl-start center">
+          <div class="search_wrapper d-flex justify-content-center align-items-center gap-3">
+            <input class="search_header" type="text" placeholder="Dimmi una Città o un Indirizzo.." v-model="query"
+              @keyup.enter="searchHomes()" @input="searchAddress" />
+            <ul v-if="addresses.length > 0">
+              <li v-for="address in addresses" @click="selectAddress(address)">
+                {{ address.address.freeformAddress }}
+              </li>
+            </ul>
+            <a class="search_button" @click="searchHomes()"><span><i
+                  class="fa-solid fa-magnifying-glass px-2"></i></span>Ricerca</a>
+          </div>
+        </div>
+        <div
+          class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4 col-xxl-4 right d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-end justify-content-xxl-end">
+          <div class="user_elements d-flex justify-content-between align-items-center gap-1">
             <a class="affitta_header" href="#">Affitta con BoolBnb</a>
             <div class="user_dropdown_wrapper">
               <div class="user_dropdown_elements">
                 <div v-if="authenticated" class="btn-group">
-                  <button
-                    type="button"
-                    class="btn user_header dropdown-toggle"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
+                  <button type="button" class="btn user_header dropdown-toggle" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
                     <i class="fa-regular fa-user px-2"></i>
                     {{ name }}
                   </button>
                   <div class="dropdown-menu dropdown_user">
-                    <a
-                      class="dropdown-item dropdown_user_item"
-                      href="http://127.0.0.1:8000/admin/homes"
-                      >Le mie Case</a
-                    >
+                    <a class="dropdown-item dropdown_user_item" href="http://127.0.0.1:8000/admin/homes">Le mie Case</a>
                   </div>
                 </div>
                 <div v-else class="btn-group">
-                  <button
-                    type="button"
-                    class="btn user_header dropdown-toggle"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
+                  <button type="button" class="btn user_header dropdown-toggle" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
                     <i class="fa-regular fa-user px-2"></i>
                     Il mio BoolBnb
                   </button>
                   <div class="dropdown-menu dropdown_user">
-                    <a
-                      class="dropdown-item dropdown_user_item"
-                      href="http://127.0.0.1:8000/login"
-                      >Login</a
-                    >
-                    <a
-                      class="dropdown-item dropdown_user_item"
-                      href="http://127.0.0.1:8000/register"
-                      >Registrati ora</a
-                    >
+                    <a class="dropdown-item dropdown_user_item" href="http://127.0.0.1:8000/login">Login</a>
+                    <a class="dropdown-item dropdown_user_item" href="http://127.0.0.1:8000/register">Registrati ora</a>
                   </div>
                 </div>
               </div>
