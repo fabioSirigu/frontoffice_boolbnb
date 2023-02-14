32242285a18bf8408aa3dbaef3a18edc5d46a5ec
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-14 15:23:32 +0100

    Fixed search view

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,13 +1,21 @@
 <script>
+import { main } from '@popperjs/core';
 import axios from 'axios'
+import { store } from '../main.js';
 
 export default {
   name: 'SearchResults',
   data() {
     return {
-      results: []
+      api_url: store.api_base_url,
+      results: [],
+      error: '',
+      max: 30
     };
   },
+  props: [
+      'filteredHomes'
+  ],
   created() {
     this.results = this.$route.query.results;
   },
@@ -31,14 +39,18 @@ export default {
 
 <template>
 <div class="search_results_wrapper">
-  <div class="search_results_elements">
+  <div class="search_results_elements py-3">
     <div class="row align-items-center align-content-start">
-        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 text-center" v-for="home in results.data">
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 text-center" v-for="home in filteredHomes">
             <div class="single_home_contents p-2">
+                <img class="home_image" :src="imageConverter(home.cover_image)">
                 <div class="card-body justify-content-center mt-2">
                     <h5 class="card_title bold card-title text-center py-3">
                         {{ trimBody(home.title) }}
                     </h5>
+                    <router-link class="home_link black" :to="{ name: 'single-home', params: { slug: home.slug } }">
+                      SCOPRI ORA
+                    </router-link>
                 </div>
             </div>
           </div>
