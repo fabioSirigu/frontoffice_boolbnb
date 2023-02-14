21196dde0ee0cdcc409fab7630b025a3dfe90570
naz1ne <fabio.sirigu.1995@gmail.com>
2023-02-14 11:14:26 +0100

    add singleHomeView

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from './views/HomeView.vue';
 import SingleHomeView from './views/SingleHomeView.vue';
+import NotFoundView from './views/NotFoundView.vue'
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -14,6 +16,11 @@ const router = createRouter({
             path: '/homes/:slug',
             name: 'single-home',
             component: SingleHomeView
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFoundView
         },
     ]
 })
