9ce0e1c1385d0a3ab5ef035264973bbe4f07868e
Yuri-Punzo <yuripunzo@hotmail.it>
2023-02-20 17:34:26 +0100

    style fixing in search result +style header shadow

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -5,127 +5,130 @@ import HomeCard from '../components/HomeCard.vue';
 import { store } from '../main.js';
 
 export default {
-    name: "SearchResults",
-    data() {
-        return {
-            api_url: store.api_base_url,
-            error: "",
-            max: 30,
-            services: [],
-            rooms: "",
-            beds: "",
-            radius: "",
-            selectedServices: [],
-            searchFilteredHomes: [],
-            filteredRoomServicesHomes: [],
-            functionActive: false,
-            latitude: this.$route.query.latitude,
-            longitude: this.$route.query.longitude
-        };
+  name: "SearchResults",
+  components: {
+    HomeCard
+  },
+  props: [
+    'filteredHomes'
+  ],
+  data() {
+    return {
+      api_url: store.api_base_url,
+      error: "",
+      max: 30,
+      services: [],
+      rooms: "",
+      beds: "",
+      radius: "",
+      selectedServices: [],
+      searchFilteredHomes: [],
+      filteredRoomServicesHomes: [],
+      functionActive: false,
+      latitude: this.$route.query.latitude,
+      longitude: this.$route.query.longitude
+    };
+  },
+  methods: {
+    imageConverter(way) {
+      //console.log(way);
+      if (way) {
+        return this.api_url + "/storage/" + way;
+      }
+      return "https://htmlcolors.com/brand-image/airbnb.png";
     },
-    props: [
-        'filteredHomes'
-    ],
-    methods: {
-        imageConverter(way) {
-            //console.log(way);
-            if (way) {
-                return this.api_url + "/storage/" + way;
-            }
-            return "https://htmlcolors.com/brand-image/airbnb.png";
-        },
-        trimBody(text) {
-            if (text.length > this.max) {
-                return text.slice(0, this.max) + "...";
-            }
-            return text;
-        },
-        async getServices() {
-            try {
-                const response = await axios.get(this.api_url + "/api/services");
-                this.services = response.data;
-            }
-            catch (error) {
-                console.error(error);
-            }
-        },
-        async filterSearch() {
-            try {
-                const response = await axios.get(this.api_url + "/api/homes");
-                let searchFilteredHomes = response.data.data;
-                console.log(searchFilteredHomes);
-                console.log(this.radius);
-                this.functionActive = true;
-                // Applica il filtro per il numero di stanze
-                if (this.rooms) {
-                    searchFilteredHomes = searchFilteredHomes.filter(home => home.rooms >= parseInt(this.rooms));
-                }
-                // Applica il filtro per il numero di letti
-                if (this.beds) {
-                    searchFilteredHomes = searchFilteredHomes.filter(home => home.beds >= parseInt(this.beds));
-                }
-                // Applica il filtro per i servizi
-                if (this.selectedServices.length > 0) {
-                    searchFilteredHomes = searchFilteredHomes.filter(home => {
-                        for (let service of this.selectedServices) {
-                            if (!home.services.some(s => s.slug === service)) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    });
-                }
-                // Applica il filtro per la distanza
-                if (this.latitude && this.longitude && this.radius) {
-                    const R = 6371; // radius of the earth in km
-                    searchFilteredHomes = searchFilteredHomes.filter(home => {
-                        const dLat = (home.latitude - this.latitude) * Math.PI / 180;
-                        const dLon = (home.longitude - this.longitude) * Math.PI / 180;
-                        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                            Math.cos(this.latitude * Math.PI / 180) * Math.cos(home.latitude * Math.PI / 180) *
-                                Math.sin(dLon / 2) * Math.sin(dLon / 2);
-                        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-                        const distance = R * c;
-                        return distance <= this.radius;
-                    });
-                }
-                this.filteredRoomServicesHomes = searchFilteredHomes;
-            }
-            catch (error) {
-                console.error(error);
-            }
-        },
-        async filterHomess() {
-            try {
-                const params = {};
-                if (this.rooms) {
-                    params.rooms = this.rooms;
-                }
-                if (this.beds) {
-                    params.beds = this.beds;
-                }
-                if (this.selectedServices.length > 0) {
-                    params.services = this.selectedServices.join(",");
-                }
-                if (this.latitude && this.longitude && this.radius) {
-                    params.latitude = this.latitude;
-                    params.longitude = this.longitude;
-                    params.radius = parseInt(this.radius);
-                }
-                const response = await axios.get(this.api_url + "/api/homes/filter/", { params });
-                this.filteredRoomServicesHomes = response.data.data;
-            }
-            catch (error) {
-                console.error(error);
-            }
+    trimBody(text) {
+      if (text.length > this.max) {
+        return text.slice(0, this.max) + "...";
+      }
+      return text;
+    },
+    async getServices() {
+      try {
+        const response = await axios.get(this.api_url + "/api/services");
+        this.services = response.data;
+      }
+      catch (error) {
+        console.error(error);
+      }
+    },
+    async filterSearch() {
+      try {
+        const response = await axios.get(this.api_url + "/api/homes");
+        let searchFilteredHomes = response.data.data;
+        console.log(searchFilteredHomes);
+        console.log(this.radius);
+        this.functionActive = true;
+        // Applica il filtro per il numero di stanze
+        if (this.rooms) {
+          searchFilteredHomes = searchFilteredHomes.filter(home => home.rooms >= parseInt(this.rooms));
         }
+        // Applica il filtro per il numero di letti
+        if (this.beds) {
+          searchFilteredHomes = searchFilteredHomes.filter(home => home.beds >= parseInt(this.beds));
+        }
+        // Applica il filtro per i servizi
+        if (this.selectedServices.length > 0) {
+          searchFilteredHomes = searchFilteredHomes.filter(home => {
+            for (let service of this.selectedServices) {
+              if (!home.services.some(s => s.slug === service)) {
+                return false;
+              }
+            }
+            return true;
+          });
+        }
+        // Applica il filtro per la distanza
+        if (this.latitude && this.longitude && this.radius) {
+          const R = 6371; // radius of the earth in km
+          searchFilteredHomes = searchFilteredHomes.filter(home => {
+            const dLat = (home.latitude - this.latitude) * Math.PI / 180;
+            const dLon = (home.longitude - this.longitude) * Math.PI / 180;
+            const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(this.latitude * Math.PI / 180) * Math.cos(home.latitude * Math.PI / 180) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            const distance = R * c;
+            return distance <= this.radius;
+          });
+        }
+        this.filteredRoomServicesHomes = searchFilteredHomes;
+      }
+      catch (error) {
+        console.error(error);
+      }
     },
-    mounted() {
-        this.getServices();
-    },
-    components: {
-      HomeCard
+    async filterHomess() {
+      try {
+        const params = {};
+        if (this.rooms) {
+          params.rooms = this.rooms;
+        }
+        if (this.beds) {
+          params.beds = this.beds;
+        }
+        if (this.selectedServices.length > 0) {
+          params.services = this.selectedServices.join(",");
+        }
+        if (this.latitude && this.longitude && this.radius) {
+          params.latitude = this.latitude;
+          params.longitude = this.longitude;
+          params.radius = parseInt(this.radius);
+        }
+        const response = await axios.get(this.api_url + "/api/homes/filter/", { params });
+        this.filteredRoomServicesHomes = response.data.data;
+      }
+      catch (error) {
+        console.error(error);
+      }
     }
+  },
+  mounted() {
+    this.getServices();
+  },
+  components: {
+    HomeCard
+  }
 }
 </script>
 
@@ -199,21 +202,9 @@ export default {
                       </div>
                     </div>
                   </div>
-                  <div class="home_results_elements">
-                    <div class="row align-items-center align-content-start">
-                      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 text-center"
-                        v-for="home in filteredRoomServicesHomes">
-                        <div class="single_home_contents p-2">
-                          <img class="home_image" :src="imageConverter(home.cover_image)">
-                          <div class="card-body justify-content-center mt-2">
-                            <h5 class="card_title bold card-title text-center py-3">
-                              {{ trimBody(home.title) }}
-                            </h5>
-                            <router-link :to="{ name: 'single-home', params: { slug: home.slug } }">Leggi di
-                              più</router-link>
-                          </div>
-                        </div>
-                      </div>
+                  <div class="home_results_elements container">
+                    <div class="row align-items-center align-content-start g-4">
+                      <HomeCard v-for="home in filteredRoomServicesHomes" :home="home" />
                     </div>
                   </div>
                 </div>
@@ -333,33 +324,9 @@ export default {
                   </div>
                 </div>
               </div>
-              <div class="home_results_elements">
-                <div class="row align-items-center align-content-start">
-                  <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 text-center py-3"
-                    v-for="home in filteredHomes">
-                    <router-link class="link-card" :to="{ name: 'single-home', params: { slug: home.slug } }">  
-                      <div class="single_home_contents p-2">
-                        <img class="home_image" :src="imageConverter(home.cover_image)">
-                        <div class="card-body justify-content-center mt-2">
-                          <h5 class="card_title bold card-title text-center py-3">
-                            {{ trimBody(home.title) }}
-                          </h5>
-                          <h5>
-                            {{ home.address }}
-                          </h5>
-                        </div>
-                        <div class="details_card">
-                          <ul>
-                            <li>{{ home.rooms }} stanze</li>
-                            <li>{{ home.beds }} letti</li>
-                            <li>{{ home.bathrooms }} bagni</li>
-                            <li>{{ home.square_meters }} mq</li>
-                          </ul>
-                          <li v-for="service in home.services">{{ service.title }}</li>
-                        </div>
-                      </div>
-                      </router-link>
-                  </div>
+              <div class="home_results_elements container">
+                <div class="row align-items-center align-content-start g-4">
+                  <HomeCard v-for="home in filteredHomes" :home="home" />
                 </div>
               </div>
             </div>
@@ -396,22 +363,22 @@ export default {
 
 .details_card {
 
-ul {
+  ul {
     display: flex;
     padding-left: 0;
     margin-bottom: 0;
     justify-content: center;
 
     li {
-        list-style: none;
-        padding: 0, 5px;
-        margin-right: 1.2rem;
-        color: gray;
+      list-style: none;
+      padding: 0, 5px;
+      margin-right: 1.2rem;
+      color: gray;
     }
 
     li:first-child {
-        list-style: none;
+      list-style: none;
     }
-}
+  }
 }
 </style>
